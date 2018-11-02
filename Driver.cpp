4aa96f02fcef1c55325214eab5095a188fe2ea75
arigl <42983251+arigl@users.noreply.github.com>
2018-11-01 23:45:21 -0700

    Add files via upload
    Quiz Fix

--- Driver.cpp
+++ Driver.cpp
@@ -5,7 +5,7 @@
  Class: CPSC 350
  Section: 2
  Assignment 4: Register Office Simulation
- Purpose: Takes in arguments for a filename and executes the rest of the program 
+ Purpose: Takes in arguments for a filename and executes the rest of the program
  */
 
 
@@ -23,12 +23,11 @@ int main(int argc, char** argv){
   Simulation s(fileName);
   s.moveLine();
 
-  cout << "The average student wait time is: " << s.meanWait << " minutes!" << endl;
+  cout << "The mean student wait time is: " << s.meanWait << endl;
   cout << "The median student wait time is: " << s.medianWait << " minutes!" << endl;
   cout << "The longest student wait time is: " << s.longestWait << " minutes!" << endl;
-  cout << s.waitedOverTen << " student(s) waited over 10 minutes" << endl;
-  cout << "" << endl;
-  cout << "Windows were idle for an average of " << s.averageIdle << " minutes!" << endl;
+  cout << "The number of students waiting over 10 minutes is: "<< s.waitedOverTen << endl;
+  cout << "The mean window idle time is:  " << s.averageIdle << endl;
   cout << "The longest window idle time is: " << s.longestIdle << endl;
-  cout << s.idleOverFive << " windows were idle for over 5 minutes" << endl;
+  cout << "Number of windows idle for over 5 minutes: "<< s.idleOverFive << endl;
 }
